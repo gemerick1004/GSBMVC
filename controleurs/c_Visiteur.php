563baef59695b0607e2e15ce49ac365924582786
gemerick1004 <d.emerick1004@gmail.com>
2013-12-19 16:14:50 +0100

    Ajout Emerick

--- controleurs/c_Visiteur.php
+++ controleurs/c_Visiteur.php
@@ -16,20 +16,18 @@ switch($action){
 		$leVisiteur = $_REQUEST['lstVisiteur']; 
                 $leMois = $_REQUEST['lstMois'];
 		$lesVisiteur=$pdo->getLesVisiteurDisponibles();
-                $leMois = $pdo->getFicheFraisVisiteur($lesVisiteur, $leMois);
+                $leMois = $pdo->getFicheFraisVisiteur($leVisiteur, $leMois);
 		$VisiteurASelectionner = $leVisiteur;
 		include("vues/v_listeVisiteur.php");
 		$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($leVisiteur,$leMois);
 		$lesFraisForfait= $pdo->getLesFraisForfait($leVisiteur,$leMois);
 		$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($leVisiteur,$leMois);
-		$numAnnee =substr( $leMois,0,4);
-		$numMois =substr( $leMois,4,2);
 		$libEtat = $lesInfosFicheFrais['libEtat'];
 		$montantValide = $lesInfosFicheFrais['montantValide'];
 		$nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
 		$dateModif =  $lesInfosFicheFrais['dateModif'];
 		$dateModif =  dateAnglaisVersFrancais($dateModif);
-		include("vues/v_etatFrais.php");
+		include("vues/v_etatFraisVisiteur.php");
 	}
 }
 ?>
